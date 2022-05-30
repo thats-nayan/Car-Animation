0df5e1024946bc0615fd410fad24d12f0e5f73c0
thats-nayan <84136203+thats-nayan@users.noreply.github.com>
2022-05-30 23:08:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Learnings
 // How to play audio in Javascript 
 
-var audio = new Audio("/Project6_Lamborgini/img/sound.mp3");
+var audio = new Audio("sound.mp3");
 audio.loop = true;
 audio.play();
